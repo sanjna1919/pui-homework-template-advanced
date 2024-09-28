79203dd18485cc003e00e66c66834ca1bd0ae315
sanjna1919 <sanjnas@andrew.cmu.edu>
2024-09-28 13:12:12 -0400

    hw4 full old

---
+++ solution-hw4/src/navbar.js
@@ -0,0 +1,35 @@
+import logo from "./assets/logo/logo-01.svg";
+
+function Navbar({items, total}){
+    return(
+        <div className="navbar-box">
+            <div className="nav-left">
+            <img src={logo} className="logo"/>
+        </div>
+
+        <div className="nav-right">
+            <div className="nav-right-top">
+                <div className="navlinks">
+                    <span className="link">
+                        <a href="" >Products</a>
+                    </span>
+                    <span className="link">
+                        <a href="" >Cart</a>
+                    </span>
+                    <div className="cart-description">
+                        <p className="bodysmall" id="items-in-cart">{items} Items</p>
+                        <p className="bodysmall" id="cart-total">Total: ${total}</p>
+                    </div>
+                </div>
+            </div>
+                <div className="nav-right-bottom">
+                    <div className="main-title">Our hand-made cinnamon rolls</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Navbar
+
